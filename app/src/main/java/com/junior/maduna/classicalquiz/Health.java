7178ac0bc5283a12888f88ee25c3c768caf83491
Cordlesswire <chungwoo007@gmail.com>
2017-06-02 12:50:05 +0200

    Initial commit

--- app/src/main/java/com/junior/maduna/classicalquiz/Health.java
+++ app/src/main/java/com/junior/maduna/classicalquiz/Health.java
@@ -1,13 +1,45 @@
 package com.junior.maduna.classicalquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Health extends AppCompatActivity {
+
+    private int mAnimationDuration = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
     }
+
+     /*
+     *  Might have to get the user to enter their name before the or a variable that will control the If statement thats
+     * going to create a if statement to control the visibility and non visibility of the app, using the variable answer not null meaning that the user is not trying to just click through the screens
+     */
+
+
+     /*Just a temporary method just to see how the app runs currently...So if there are UI errors I can fix them
+      *
+      */
+
+
+         public void questionOne(View view) {
+             final View hideQuestionOne = findViewById(R.id.linearlayout_question1);
+             View showQuestionTwo = findViewById(R.id.linearlayout_question2);
+             showQuestionTwo.setVisibility(View.GONE);
+             mAnimationDuration = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+
+             // set the appearing view to 0% opacity but visible, so that it is visible (but fully transparent) during the animation
+             showQuestionTwo.setAlpha(0f);
+             showQuestionTwo.setVisibility(View.VISIBLE);
+             hideQuestionOne.setVisibility(View.GONE);
+
+
+
+         }
+
 }
--- app/src/main/java/com/junior/maduna/classicalquiz/Score.java
+++ app/src/main/java/com/junior/maduna/classicalquiz/Score.java
@@ -1,13 +1,71 @@
 package com.junior.maduna.classicalquiz;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Score extends AppCompatActivity {
+
+    int correctAnswers = 0;
+    int wrongAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
     }
+
+     /*Eish there are actualy two submit buttons at the moment..one to submit score one to submit username,thus make one submitScore the other submitName
+     * This will make coding easier when the code starts getting heavy, the below submit is to get the users name
+     */
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitName(View view) {
+        // Get user's name
+        EditText nameField = (EditText) findViewById(R.id.user_name);
+        Editable nameEditable = nameField.getText();
+        String name = nameEditable.toString();
+
+
+                /**Took this code from the just Java App...modify it so you can use with the radioButtons a user clicks so that we know if the user hasn't submitted an anser to make the screen visible or not
+                 * // Figure out if the user wants choclate topping
+                      CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+                      boolean hasChocolate = chocolateCheckBox.isChecked();
+                 */
+
+
+
+
+
+
+    }
+
+
+       /*
+     *  ---- BUTTON - REPLAY APP ----
+     */
+
+    public void replayApp(View view){
+        correctAnswers = 0;
+        wrongAnswers = 0;
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+   /*
+     * When Exit Button is clicked the App Exits
+     *    Its not user friendly to just exit an app so ask the user first if they want to exit using a Notification
+     */
+   public void exitApp(View view){
+       finish();
+       System.exit(0);
+   }
+
 }

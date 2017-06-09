9c89700a5812adc059535e950eaf1d3eed664118
Cordlesswire <chungwoo007@gmail.com>
2017-06-09 12:22:09 +0200

    Initial Commit

--- app/src/main/java/com/junior/maduna/classicalquiz/Score.java
+++ app/src/main/java/com/junior/maduna/classicalquiz/Score.java
@@ -2,6 +2,7 @@ package com.junior.maduna.classicalquiz;
 
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -18,9 +19,22 @@ public class Score extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_score);
 
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        myThread.start();
+
     }
-
-
-
 
 }

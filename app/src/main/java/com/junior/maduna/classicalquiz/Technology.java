18dc88b3fdc1673a072d2652b11864fdca820c1b
Cordlesswire <chungwoo007@gmail.com>
2017-06-09 11:57:13 +0200

    Initial Commit

--- app/src/main/java/com/junior/maduna/classicalquiz/Technology.java
+++ app/src/main/java/com/junior/maduna/classicalquiz/Technology.java
@@ -3,9 +3,9 @@ package com.junior.maduna.classicalquiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
@@ -14,12 +14,10 @@ import android.widget.Toast;
 
 public class Technology extends AppCompatActivity {
     float score = 0;
-    float total = 3;
+    float total = 5;
     float percent = 0;
     float incorrect = 0;
 
-
-    Editable userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +35,12 @@ public class Technology extends AppCompatActivity {
         startActivity(i);
     }
 
+
+    //Display question 2
     //Find out to check if has selected a choice or not and if not display a toast message telling the user to select something before moving on
     public void questionTwo(View view) {
         RadioGroup radioGQ1 = (RadioGroup) findViewById(R.id.tradioGroupOne);
+        //check if the user has selected something if not dont go to the next question
         if (radioGQ1.getCheckedRadioButtonId() != -1) {
             ScrollView layout = (ScrollView) findViewById(R.id.question2);
             ScrollView layout1 = (ScrollView) findViewById(R.id.question1);
@@ -49,11 +50,13 @@ public class Technology extends AppCompatActivity {
             Toast.makeText(Technology.this,
                     "PLEASE SELECT AN OPTION BEFORE MOVING ON TO THE NEXT QUESTION", Toast.LENGTH_SHORT).show();
         }
+
     }
 
-    //Find out to check if has selected a choice or not and if not display a toast message telling the user to select something before moving on
+    //Display question 3
     public void questionThree(View view) {
         RadioGroup radioGQ2 = (RadioGroup) findViewById(R.id.tradioGroupTwo);
+        //check if the user has selected something if not dont go to the next question
         if (radioGQ2.getCheckedRadioButtonId() != -1) {
             ScrollView layout = (ScrollView) findViewById(R.id.question3);
             ScrollView layout1 = (ScrollView) findViewById(R.id.question2);
@@ -65,15 +68,32 @@ public class Technology extends AppCompatActivity {
         }
     }
 
+
+    //Display question 4
+    public void questionFour(View view) {
+        ScrollView layout = (ScrollView) findViewById(R.id.question4);
+        ScrollView layout1 = (ScrollView) findViewById(R.id.question3);
+        layout1.setVisibility(View.GONE);
+        layout.setVisibility(View.VISIBLE);
+    }
+
+
+    //Display question 5
+    public void questionFive(View view) {
+        ScrollView layout = (ScrollView) findViewById(R.id.question5);
+        ScrollView layout1 = (ScrollView) findViewById(R.id.question4);
+        layout1.setVisibility(View.GONE);
+        layout.setVisibility(View.VISIBLE);
+    }
+
+
     //New Score method
     public void scoreDisplay(View view) {
-
-
         RadioGroup radioGQ3 = (RadioGroup) findViewById(R.id.tradioGroupThree);
         //check if the user has selected something if not dont go to the next question
         if (radioGQ3.getCheckedRadioButtonId() != -1) {
             ScrollView layout = (ScrollView) findViewById(R.id.techScore);
-            ScrollView layout1 = (ScrollView) findViewById(R.id.question3);
+            ScrollView layout1 = (ScrollView) findViewById(R.id.question5);
             layout1.setVisibility(View.GONE);
             layout.setVisibility(View.VISIBLE);
 
@@ -111,7 +131,33 @@ public class Technology extends AppCompatActivity {
 
                 }
 
+
             }
+
+            //Get the users selection from the checkbox and decide if its the correct one and increment score
+            CheckBox newton = (CheckBox) findViewById(R.id.techBox1);
+            boolean isNewton = newton.isChecked();
+
+            CheckBox pascal = (CheckBox) findViewById(R.id.techBox2);
+            boolean isPascal = pascal.isChecked();
+
+            CheckBox kepler = (CheckBox) findViewById(R.id.techBox3);
+            boolean isKepler = kepler.isChecked();
+
+            if( isPascal&& !isKepler && !isNewton){
+                score++;
+            }
+
+            //Get the users input from the edittext and decide if its the correct one and increment score
+            EditText artistInput = (EditText) findViewById(R.id.tech);
+            String artistName = artistInput.getText().toString();
+
+            if (artistName.equalsIgnoreCase("Johannes Gutenburg")) {
+                score++;
+            }
+
+
+
 
             TextView quizScore = (TextView) findViewById(R.id.mscore);
             percent = (score * 100) / total;
